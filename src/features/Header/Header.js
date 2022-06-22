117fcded4998203457e55e4d6f439ef73971ffab
kjoling <jolingkw@gmail.com>
2022-06-21 17:12:58 -0700

    add search and styles to header

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,21 +1,33 @@
 import React from "react";
 import RedditIcon from "@mui/icons-material/Reddit";
 import SearchIcon from "@mui/icons-material/Search";
+import Paper from "@mui/material/Paper";
+import { TextField } from "@mui/material";
+
+import "./Header.css";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <RedditIcon />
-        <p>
-          Reddit<span>Minimal</span>
-        </p>
-      </div>
-      <form className="search">
-        <input type="text" placeholder="Search" />
-        <SearchIcon />
-      </form>
-    </header>
+    <Paper>
+      <header>
+        <div className="redditLogo">
+          <RedditIcon />
+          <p className="redditSiteName">
+            Reddit <span className="redditMinimal">Minimal</span>
+          </p>
+        </div>
+        <form className="search">
+          <TextField
+            id="standard-search"
+            label="Search"
+            type="search"
+            variant="standard"
+            sx={{ margin: "0" }}
+          />
+          <SearchIcon sx={{ cursor: "pointer" }} />
+        </form>
+      </header>
+    </Paper>
   );
 };
 
